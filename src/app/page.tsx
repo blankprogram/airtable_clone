7ca41984545181e5161cc443f360aa3cb4e326a6
Jamal Elmir <blankfragz@gmail.com>
2024-12-03 20:04:45 +1100

    type safety

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { LandingPage } from "~/app/_components/landing";
 import { auth } from "~/server/auth";
 import { HydrateClient } from "~/trpc/server";
+import type { Session } from "next-auth";
 
 export default async function Home() {
   const session = await auth();
@@ -13,12 +14,12 @@ export default async function Home() {
   );
 }
 
-function LoggedInLanding({ session }: { session: any }) {
+function LoggedInLanding({ session }: { session: Session }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Welcome Back, {session.user.name}!
+          Welcome Back, {session.user?.name ?? "User"}!
         </h1>
         <Link
           href="/dashboard"
